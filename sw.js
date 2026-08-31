c4827439ca54ca582a5db3d3de70f9da16ed2727
Abhijith Sivaprasadan <63243381+Aintnopsychopath@users.noreply.github.com>
2026-08-31 19:44:30 +0200

    docs(gb-flexabm): publish actual historical market coverage gaps

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
  * To disable for a session, the user can hit `?nosw=1` once.
  */
 
-const VERSION = "v4-w25-20260831-gb-data";
+const VERSION = "v4-w26-20260831-gb-market";
 const SHELL_CACHE = `shell-${VERSION}`;
 const RUNTIME_CACHE = `runtime-${VERSION}`;
 const NETWORK_FIRST_EXTENSIONS = /\.(?:css|js|json)$/i;
